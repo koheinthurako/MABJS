46ba279b075738d4442b3d99b43ebf19917fc2aa
koheinthurako <captainthura99@gmail.com>
2023-09-17 21:37:08 +0630

    add details 1

--- knowledge.js
+++ knowledge.js
@@ -1,18 +1,59 @@
-const a = 5;
-const b = "hein";
+// GEC & FEC
+// const a = 5;
+// const b = "hein";
  
-const af = () => {
-    const x = 10;
-    const y = 20;
-    const z = x + y;
-    return "this is af " + z;
-};
+// const af = () => {
+//     const x = 10;
+//     const y = 20;
+//     const z = x + y;
+//     return "this is af " + z;
+// };
 
-function fs() {
-    const j = "jjj";
-    const k = "kkk";
-    return "this is fs " + j + k;
-}
+// function fs() {
+//     const j = "jjj";
+//     const k = "kkk";
+//     return "this is fs " + j + k;
+// }
 
-af();
-fs();
+// af();
+// fs();
+
+
+// sync & async
+// function sync() {
+//     console.log("a");    
+//     for(let i = 1; i<=10; i++) {
+//         console.log(i, "b");
+//     }
+//     console.log("b");    
+//     console.log("c");    
+// }
+
+// sync();
+
+
+// function async() {
+//     fetch('https://dummyjson.com/products/1')
+//     .then(res => res.json())
+//     .then(console.log);
+
+//     fetch('https://dummyjson.com/products/2')
+//     .then(res => res.json())
+//     .then(console.log);
+
+//     fetch('https://dummyjson.com/products/3')
+//     .then(res => res.json())
+//     .then(console.log);
+
+//     fetch('https://dummyjson.com/products/4')
+//     .then(res => res.json())
+//     .then(console.log);
+
+//     fetch('https://dummyjson.com/products/5')
+//     .then(res => res.json())
+//     .then(console.log);
+// }
+
+// async();
+
+// console.log("this is end");
