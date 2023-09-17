3ed52bad443794f5f30d3747d43fe9effff63292
koheinthurako <captainthura99@gmail.com>
2023-09-17 22:29:08 +0630

    add details 2

--- knowledge.js
+++ knowledge.js
@@ -19,6 +19,8 @@
 // fs();
 
 
+
+
 // sync & async
 // function sync() {
 //     console.log("a");    
@@ -30,7 +32,6 @@
 // }
 
 // sync();
-
 
 // function async() {
 //     fetch('https://dummyjson.com/products/1')
@@ -57,3 +58,81 @@
 // async();
 
 // console.log("this is end");
+
+
+
+
+// Callstack, WebAPI, Queue, Queue, Event loop
+// JS က single-threaded language ဖြစ်လို့ callstack တစ်ခုပဲပါတယ် Callstack မအားဘူးဆိုရင် Queue ထဲက အလုပ်တွေအကုန် Callstack အားအောင်စောင့်ရတယ်
+// function run() {
+//     console.log("a");
+
+//     setTimeout(() => {
+//         console.log("b");
+//     }, 1);
+
+//     let z = 0;
+//     for(let i = 1; i <= 100000; i++) {
+//         z += i;
+//     }
+
+//     console.log(z);
+
+//     console.log("c");
+// }
+
+// run();
+
+
+
+
+// try ... catch ... finally
+// const i = "i";
+// console.log("a");
+
+// try {
+//     i = "u";
+// } catch (e) {
+//     console.dir(e);
+// } finally {
+//     console.log("final");
+//     // Finally က ကျိန်းသေပေါက်အလုပ်လုပ်တယ်
+// }
+
+// console.log("b");
+// console.log("c");
+
+
+
+
+// Promise approximately Async
+// promise က JS built-in-object ထဲမှာပါတယ် async ကို handle လုပ်ဖို့ တစ်ခုတည်းသော object
+// promise က sync Syntax တွေကို ထိမ်းမယ့် object
+// const p = new Promise((resolve, reject) => {
+//     // if(true) {
+//     //     // resolve is function that return data;
+//     //     resolve("Success");
+//     // } else {
+//     //     // reject is function that return data;
+//     //     reject("Fail");
+//     // }
+
+//     setTimeout(() => {
+//         const num = Math.floor(Math.random() * 10);
+//         if(num >= 5) {
+//             resolve(num);
+//         } else {
+//             reject(num);
+//         }
+//     }, 3000);
+
+// });
+// // promise ထဲက constructor ထဲမှာ Callback function တစ်ခုရှိတယ် function ထဲက parameter ထဲမှာ resolve & reject function နှစ်ခုရှိတယ်
+// // resolve က အောင်မြင်သွားခဲ့ရင် အလုပ်လုပ်မယ် reject က မအောင်မြင်ရင် အလုပ်လုပ်မယ်
+
+// console.log(p);
+
+// // resolve ထဲက တန်ဖိုးသည် then ထဲကိုရောက်လာမယ်
+// p.then((data) => console.log("Success"));
+// // reject  ထဲက တန်ဖိုးသည် catch ထဲကိုရောက်လာမယ်
+// p.catch((data) => console.log("Fail"));
