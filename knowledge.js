bbde0df4378cfaaff48f1708200165e8559cc989
koheinthurako <captainthura99@gmail.com>
2023-09-17 21:14:48 +0630

    start point

---
+++ knowledge.js
@@ -0,0 +1,18 @@
+const a = 5;
+const b = "hein";
+ 
+const af = () => {
+    const x = 10;
+    const y = 20;
+    const z = x + y;
+    return "this is af " + z;
+};
+
+function fs() {
+    const j = "jjj";
+    const k = "kkk";
+    return "this is fs " + j + k;
+}
+
+af();
+fs();
